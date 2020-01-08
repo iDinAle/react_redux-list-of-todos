9f97054c2366c62b10366de6c5d7091c44395c1d
Alexii Aidinov <alexii.aidinov@gmail.com>
2020-01-08 21:54:23 +0200

    Updates

---
+++ precache-manifest.c381965932ed7158cec45b3a37c067ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b601189cabb0e020f038da1e26e6c94",
+    "url": "/react_redux-list-of-todos/index.html"
+  },
+  {
+    "revision": "7ebd6745135bfbfe0d0f",
+    "url": "/react_redux-list-of-todos/static/css/main.e1175a0a.chunk.css"
+  },
+  {
+    "revision": "9d951fd8fedb3f7ccef5",
+    "url": "/react_redux-list-of-todos/static/js/2.46fdb100.chunk.js"
+  },
+  {
+    "revision": "7ebd6745135bfbfe0d0f",
+    "url": "/react_redux-list-of-todos/static/js/main.8358cf8f.chunk.js"
+  },
+  {
+    "revision": "37dc44999646af065e8e",
+    "url": "/react_redux-list-of-todos/static/js/runtime~main.054cc81e.js"
+  }
+]);
